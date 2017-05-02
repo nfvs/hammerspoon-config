59038c451da96fb1bbc09a0d46c97aaec5a6e734
Nuno Santos <nunofvsantos@gmail.com>
2017-05-02 15:17:06 +0200

    When moving windows between screens, maintain size and ensure they are within screen bounds.

--- position.lua
+++ position.lua
@@ -7,27 +7,27 @@ hs.window.animationDuration = 0
 hs.hotkey.bind(hypershift, "left", function()
   local win = hs.window.focusedWindow()
   if (win) then
-    win:moveOneScreenWest()
+    win:moveOneScreenWest(true, true)
   end
 end)
 
 hs.hotkey.bind(hypershift, "right", function()
   local win = hs.window.focusedWindow()
   if (win) then
-    win:moveOneScreenEast()
+    win:moveOneScreenEast(true, true)
   end
 end)
 
 hs.hotkey.bind(hypershift, "up", function()
   local win = hs.window.focusedWindow()
   if (win) then
-    win:moveOneScreenNorth()
+    win:moveOneScreenNorth(true, true)
   end
 end)
 
 hs.hotkey.bind(hypershift, "down", function()
   local win = hs.window.focusedWindow()
   if (win) then
-    win:moveOneScreenSouth()
+    win:moveOneScreenSouth(true, true)
   end
 end)
